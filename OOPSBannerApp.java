10a64821915f1673ec5788ff7ba7675aa2c72670
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-03 13:10:32 +0530

    Refactor OOPSBannerApp to display a banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+class OOPSBannerApp {
+
+  public static void main(String[] args) {
+
+    String[] banner = {
+        String.join("   ", " ***** ", " ***** ", " ****** ", " ***** "),
+        String.join("   ", "*     *", "*     *", "*     * ", "*     "),
+        String.join("   ", "*     *", "*     *", "*     * ", "*     "),
+        String.join("   ", "*     *", " ***** ", " ****** ", " ***** "),
+        String.join("   ", "*     *", "*       ", "*       ", "      *"),
+        String.join("   ", "*     *", "*       ", "*       ", "      *"),
+        String.join("   ", " ***** ", "*       ", "*       ", "******  ")
+    };
+
+    for (String line : banner) {
+      System.out.println(line);
     }
+  }
 }
