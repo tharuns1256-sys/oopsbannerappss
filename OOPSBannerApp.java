4d827e0f6b0d2ba5c581c98811981e292858b4f5
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-03 14:57:16 +0530

    UC08 Done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,66 @@
-public class OOPSBannerApp {
-  public static void main(String[] args) {
-    System.out.println("  ***   " + "  ***   " + " *****  " + "  **** ");
-    System.out.println(" **  ** " + " **  ** " + " **  ** " + " **     ");
-    System.out.println(" **  ** " + " **  ** " + " *****  " + " *****  ");
-    System.out.println(" **  ** " + " **  ** " + " **     " + "     ** ");
-    System.out.println("  ***   " + "  ***   " + " **     " + " ****git*  ");
-  }
+import java.util.HashMap;
+import java.util.Map;
 
+public class OOPSBannerApp {
+    public static Map<Character, String[]> buildCharacterMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+    public static void renderBanner(String word, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = map.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> characterMap = buildCharacterMap();
+
+        renderBanner("OOPS", characterMap);
+    }
 }
