d12e85313bc1a49a184397eeebd18e28ee815381
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-03 13:35:14 +0530

    UC06: Implemented changes

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,59 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+class OOPSBannerApp {
+
+  public static void main(String[] args) {
+
+    String[] oPattern = getOPattern();
+    String[] pPattern = getPPattern();
+    String[] sPattern = getSPattern();
+
+    String[] banner = new String[7];
+
+    for (int i = 0; i < 7; i++) {
+      banner[i] = String.join("   ",
+          oPattern[i],
+          oPattern[i],
+          pPattern[i],
+          sPattern[i]);
     }
+
+    for (String line : banner) {
+      System.out.println(line);
+    }
+  }
+
+  public static String[] getOPattern() {
+    return new String[] {
+        " ***** ",
+        "*     *",
+        "*     *",
+        "*     *",
+        "*     *",
+        "*     *",
+        " ***** "
+    };
+  }
+
+  public static String[] getPPattern() {
+    return new String[] {
+        " ******",
+        "*     *",
+        "*     *",
+        " ******",
+        "*      ",
+        "*      ",
+        "*      "
+    };
+  }
+
+  public static String[] getSPattern() {
+    return new String[] {
+        " ***** ",
+        "*     *",
+        "*      ",
+        " ***** ",
+        "      *",
+        "*     *",
+        " ***** "
+    };
+  }
 }
