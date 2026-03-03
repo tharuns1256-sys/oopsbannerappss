c6a31f57b9eeb4dfc4a7815eb98a8cdccc9cff30
tharuns1256-sys <tharuns1256@gmail.com>
2026-03-03 11:11:09 +0530

    UC03 Done

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,9 @@
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
+  public static void main(String[] args) {
+    System.out.println(String.join(" ", " ***  ", " ***  ", " ***** ", " ***** "));
+    System.out.println(String.join(" ", "**  **", "**  **", "**  ** ", "**     "));
+    System.out.println(String.join(" ", "**  **", "**  **", "*****  ", " ***** "));
+    System.out.println(String.join(" ", "**  **", "**  **", "**     ", "    ** "));
+    System.out.println(String.join(" ", " ***  ", " ***  ", "**     ", " ***** "));
+  }
 }
